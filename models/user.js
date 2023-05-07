554d0c7d83323d79b7994706d95248c377d70404
Your Name <borsinileuro@gmail.com>
2023-05-07 13:53:47 +0200

    eyesGlasses mongoDb online

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
+import mongoose from "mongoose"
 const userSchema=mongoose.Schema({
     nome:{type:String, require:true},
     gender:{type:String, require:true},
     materiale:{type:String, require:true}
 }, {timestamps:true})
-export const User = mongoose.model('User',userSchema)
+export const Occhiale = mongoose.model('prodotti',userSchema)
--- routes/occhiali.js
+++ routes/occhiali.js
@@ -1,41 +1,48 @@
 import express from 'express'
 import {v4 as uuidv4} from 'uuid'
+import {Occhiale} from '../models/user.js'
+import mongoose from 'mongoose'
 
 const router = express.Router()
 
 let occhiali = [{id:"1",nome:"bianco",gender:"maschio",materiale:"plastica"},
                 {id:"2",nome:"verde",gender:"maschio",materiale:"titanio"}]
-  
-router.get('/',(req,res)=>{ 
-    res.json(occhiali)
-    console.log(occhiali)})
+occhiali=[]
+router.get('/',async(req,res)=>{ 
+    try{const occhiali=await Occhiale.find()
+    res.status(200).json(occhiali)}
+    catch(error){res.status(404).json({message:error.message})}})
 
-router.post('/',(req,res)=>{
+router.post('/',async(req,res)=>{
     const occhiale=req.body
+    const newOcchiale=new Occhiale(occhiale)
     console.log(occhiale)
-    occhiali.push({...occhiale, id:uuidv4()})
-    res.send('Occhiale aggiunto con successo<br> Premi <a href="catalogo.html">qui</a> per la homepage')})
+    try {await newOcchiale.save()
+    res.send('Occhiale aggiunto con successo<br> Premi <a href="catalogo.html">qui</a> per la homepage')}
+    catch (error) {res.status(409).json({message:error.message})}})
 
-router.delete('/:id',(req,res)=>{
+router.delete('/:id',async(req,res)=>{
     const{id}=req.params
-    const index=occhiali.findIndex(occhiale=>occhiale.id===id)
-    occhiali.splice(index,1)
-    res.status(200).json({success:true, data:occhiali})})
+    if (!mongoose.Types.ObjectId.isValid(id))
+    {return res.status(404).json({message:'id non valido'})}
+    try {await Occhiale.findByIdAndDelete(id)
+    res.json({message:'utente eliminato con successo<br><a href="../catalogo.html>Torna alla home</a>'})}
+    catch (error) {res.status(404).json({message:error.message})}})
 
-router.post('/:id',(req,res)=>{
+router.post('/:id',async(req,res)=>{
     const {id} = req.params
-    const newOcchiale=req.body
-    for (let i=0; i<occhiali.length; i++) {
-        let occhiale=occhiali[i]
-        if (occhiale.id==id) {
-            occhiali[i]=newOcchiale}}
-    res.send(`occhiale con id ${id} modificato con successo!<br>
-        <a href="../catalogo.html">Torna al catalogo</a>`)})
+    const newOcchiale={...req.body}
+    if (!mongoose.Types.ObjectId.isValid(id))
+    {return res.status(404).json({message:'id non presente'})}
+    try { 
+        const occhiale=await Occhiale.findByIdAndUpdate(id,newOcchiale,{new:true})
+        res.send(`occhiale con id ${occhiale._id} modificato con successo<br><a href="../catalogo.html">Torna alla home</a>`)}
+    catch (error) {res.status(404).json({message:error.message})}})
 
-router.get('/:id',(req,res)=>{
+router.get('/:id',async(req,res)=>{
     const {id}=req.params
-    const occhiale=occhiali.find((occhiale)=>occhiale.id===id)
-    if (occhiale) {res.send(occhiale)}
-    else {res.status(404).send('occhiale non trovato')}
-})
+    try{const occhiale=await Occhiale.findById(id)
+    res.status(200).json(occhiale)}
+    catch (error){res.status(404).json({message: error.message})}})
+
 export default router
